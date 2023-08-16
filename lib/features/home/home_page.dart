4b1669a00f4240d694dedf0fdda520fdcbcb7aec
Viachaslau Pratasenia <viachaslau.pratasenia@gmail.com>
2023-08-16 22:59:51 +0200

    added stack for all scroll areas

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -14,12 +14,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.colors.background,
-      body: Row(
+      body: Stack(
         children: [
-          SizedBox(width: MediaQuery.sizeOf(context).width * 0.05),
-          Expanded(
-            flex: 4,
-            child: SizedBox(
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 56),
+              width: MediaQuery.sizeOf(context).width * 0.4,
               height: MediaQuery.sizeOf(context).height,
               child: Center(
                 child: Column(
@@ -45,59 +46,63 @@ class HomePage extends StatelessWidget {
                     const SizedBox(height: 56),
                     const LanguagesBlock(),
                     const Spacer(),
-                    const SocialButtons(),
-                    const SizedBox(height: 56),
                   ],
                 ),
               ),
             ),
           ),
-          SizedBox(width: MediaQuery.sizeOf(context).width * 0.06),
-          Expanded(
-            flex: 6,
-            child: SizedBox(
-              width: double.infinity,
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  const SizedBox(height: 56),
-                  Text(
-                    'I am a highly skilled software engineer with 5 years of commercial experience in building top-notch applications for Android, iOS, MacOS, Windows and Web platforms using Flutter.\n\nI have a strong passion for creating robust and user-friendly applications that exceed client’s expectations. Throughout my career, I have gained expertise in various aspects of software development, including design, development, testing, and maintenance.\n\nMy deep understanding of programming languages, frameworks, and tools, coupled with my problem-solving skills, has enabled me to deliver complex projects within tight deadlines.',
-                    style: AppTheme.typography.semiLarge.regular.textBase,
-                  ),
-                  const SizedBox(height: 80),
-                  const WorkItem(
-                    from: 'SEPT 2022',
-                    to: 'PRESENT',
-                    title: 'Flutter Developer - Aventus IT',
-                    subtitle:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut rutrum risus, eu posuere neque. Duis urna erat, maximus sit amet ullamcorper sed, tempor id metus. Aenean a tellus at ex eleifend convallis nec et ligula. Etiam aliquet euismod fermentum. Fusce suscipit et felis a consequat. Integer sem lacus, ultrices sed ipsum sit amet, ultrices tristique velit.',
-                    skills: ['Flutter', 'Dart', 'Kotlin', 'Swift', 'Java'],
-                  ),
-                  const SizedBox(height: 32),
-                  const WorkItem(
-                    from: 'SEPT 2021',
-                    to: 'SEPT 2022',
-                    title: 'Flutter Developer - Symfa',
-                    subtitle:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut rutrum risus, eu posuere neque. Duis urna erat, maximus sit amet ullamcorper sed, tempor id metus. Aenean a tellus at ex eleifend convallis nec et ligula. Etiam aliquet euismod fermentum. Fusce suscipit et felis a consequat. Integer sem lacus, ultrices sed ipsum sit amet, ultrices tristique velit.',
-                    skills: ['Flutter', 'Dart', 'React Native'],
-                  ),
-                  const SizedBox(height: 32),
-                  const WorkItem(
-                    from: 'APR 2019',
-                    to: 'SEPT 2021',
-                    title: 'Android/Flutter Developer - HEAPIX',
-                    subtitle:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut rutrum risus, eu posuere neque. Duis urna erat, maximus sit amet ullamcorper sed, tempor id metus. Aenean a tellus at ex eleifend convallis nec et ligula. Etiam aliquet euismod fermentum. Fusce suscipit et felis a consequat. Integer sem lacus, ultrices sed ipsum sit amet, ultrices tristique velit.',
-                    skills: ['Android', 'Java', 'Kotlin', 'Flutter', 'Dart'],
-                  ),
-                  const SizedBox(height: 80),
-                ],
+          SingleChildScrollView(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                padding: const EdgeInsets.only(right: 56),
+                width: MediaQuery.sizeOf(context).width * 0.55,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    const SizedBox(height: 56),
+                    Text(
+                      'I am a highly skilled software engineer with 5 years of commercial experience in building top-notch applications for Android, iOS, MacOS, Windows and Web platforms using Flutter.\n\nI have a strong passion for creating robust and user-friendly applications that exceed client’s expectations. Throughout my career, I have gained expertise in various aspects of software development, including design, development, testing, and maintenance.\n\nMy deep understanding of programming languages, frameworks, and tools, coupled with my problem-solving skills, has enabled me to deliver complex projects within tight deadlines.',
+                      style: AppTheme.typography.semiLarge.regular.textBase,
+                    ),
+                    const SizedBox(height: 80),
+                    const WorkItem(
+                      from: 'SEPT 2022',
+                      to: 'PRESENT',
+                      title: 'Flutter Developer - Aventus IT',
+                      subtitle:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut rutrum risus, eu posuere neque. Duis urna erat, maximus sit amet ullamcorper sed, tempor id metus. Aenean a tellus at ex eleifend convallis nec et ligula. Etiam aliquet euismod fermentum. Fusce suscipit et felis a consequat. Integer sem lacus, ultrices sed ipsum sit amet, ultrices tristique velit.',
+                      skills: ['Flutter', 'Dart', 'Kotlin', 'Swift', 'Java'],
+                    ),
+                    const SizedBox(height: 32),
+                    const WorkItem(
+                      from: 'SEPT 2021',
+                      to: 'SEPT 2022',
+                      title: 'Flutter Developer - Symfa',
+                      subtitle:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut rutrum risus, eu posuere neque. Duis urna erat, maximus sit amet ullamcorper sed, tempor id metus. Aenean a tellus at ex eleifend convallis nec et ligula. Etiam aliquet euismod fermentum. Fusce suscipit et felis a consequat. Integer sem lacus, ultrices sed ipsum sit amet, ultrices tristique velit.',
+                      skills: ['Flutter', 'Dart', 'React Native'],
+                    ),
+                    const SizedBox(height: 32),
+                    const WorkItem(
+                      from: 'APR 2019',
+                      to: 'SEPT 2021',
+                      title: 'Android/Flutter Developer - HEAPIX',
+                      subtitle:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut rutrum risus, eu posuere neque. Duis urna erat, maximus sit amet ullamcorper sed, tempor id metus. Aenean a tellus at ex eleifend convallis nec et ligula. Etiam aliquet euismod fermentum. Fusce suscipit et felis a consequat. Integer sem lacus, ultrices sed ipsum sit amet, ultrices tristique velit.',
+                      skills: ['Android', 'Java', 'Kotlin', 'Flutter', 'Dart'],
+                    ),
+                    const SizedBox(height: 80),
+                  ],
+                ),
               ),
             ),
           ),
-          SizedBox(width: MediaQuery.sizeOf(context).width * 0.05),
+          const Positioned(
+            bottom: 56,
+            left: 56,
+            child: SocialButtons(),
+          )
         ],
       ),
     );
