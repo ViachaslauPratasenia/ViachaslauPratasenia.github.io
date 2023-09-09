7a57585e5b8f073ccb406829839165546725d101
Viachaslau Pratasenia <viachaslau.pratasenia@7vpn.com>
2023-09-09 16:33:09 +0200

    moved dio to locator

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:personal_website/const/const.dart';
 import 'package:personal_website/features/home/data/repository/local_profile_repository.dart';
 import 'package:personal_website/features/home/data/repository/remote_profile_repository.dart';
 import 'package:personal_website/features/home/domain/use_case/profile_cubit.dart';
@@ -6,7 +8,11 @@ import 'package:personal_website/features/home/domain/use_case/profile_cubit.dar
 GetIt locator = GetIt.instance;
 
 Future setupLocator() async {
-  locator.registerLazySingleton<RemoteProfileRepository>(() => RemoteProfileRepository());
+  locator.registerLazySingleton(() => Dio(BaseOptions(baseUrl: Const.config.GITHUB_URL)));
+
+  locator.registerLazySingleton<RemoteProfileRepository>(
+    () => RemoteProfileRepository(locator.get<Dio>()),
+  );
   locator.registerLazySingleton<LocalProfileRepository>(() => LocalProfileRepository());
 
   locator.registerFactory<ProfileCubit>(
--- lib/features/home/data/repository/remote_profile_repository.dart
+++ lib/features/home/data/repository/remote_profile_repository.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
-import 'package:personal_website/const/const.dart';
 import 'package:personal_website/features/home/data/developer_profile.dart';
 
 class RemoteProfileRepository {
+  final Dio dio;
+
+  RemoteProfileRepository(this.dio);
+
   Future<DeveloperProfile?> getProfile() async {
     try {
-      final res = await Dio().get(Const.config.PROFILE_URL);
+      final res = await dio.get('profile.json');
 
       if (res.data == null || res.data!.isEmpty) {
         return null;
