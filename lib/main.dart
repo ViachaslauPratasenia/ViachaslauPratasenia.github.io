85808e826d49426c8a193187d13696a85a766004
Viachaslau Pratasenia <viachaslau.pratasenia@7vpn.com>
2023-09-28 22:42:54 +0200

    added firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_website/app.dart';
 import 'package:personal_website/core/locator.dart';
+import 'package:personal_website/firebase_options.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   await setupLocator();
   setPathUrlStrategy();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
